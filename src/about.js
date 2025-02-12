beec2d2f477529edba647bfc0cb8ce2dcdd43f21
xccyxx <100075841+xccyxx@users.noreply.github.com>
2025-02-11 23:58:42 -0500

    Refactor about.js and add classes to buttons in template.html

--- src/about.js
+++ src/about.js
@@ -13,18 +13,24 @@ export default function() {
         }
 
         if (container === "details") {
-            const rows = [
+            const contentLines = [
                 "For any inquiries or reservations, feel free to reach out to us:", 
-                "Phone: +1 (415) 555-1234",
-                "Email: contact@oceanbreezesushi.com",
-                "Address:",
+                "<strong>Phone:</strong> +1 (415) 555-1234",
+                "<strong>Email:</strong> contact@oceanbreezesushi.com",
+                "<strong>Address:</strong>",
                 "Ocean Breeze Sushi",
                 "123 Ocean Blvd,",
                 "San Francisco, CA 94110,",
                 "United States"
             ]
             const details = document.createElement("div");
-            rows.forEach(row => {
+            contentLines.forEach(contentLine => {
+                const row = document.createElement("p");
+                if (contentLine.startsWith("<strong>")) {
+                    row.innerHTML = contentLine;
+                } else {
+                    row.textContent = contentLine;
+                }
                 details.append(row);
             })
             containerDiv.append(details);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { initializeHomeContent } from "./home.js";
 import initializeMenuContent from "./menu.js";
 import initializeAboutContent from "./about.js";
 
-    
+// set up buttons
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
 // Reset content div
 content.replaceChildren();
 
